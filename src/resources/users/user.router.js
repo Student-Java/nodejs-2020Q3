8e21fd54e1d6d7931b8912ca808e8f45ba3bb16d
Student-Java <energoslav@gmail.com>
2020-04-04 13:57:14 -0300

    feat: add user logic

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,10 +2,47 @@ const router = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+router.route('/').get(
+  wrapAsync(async (req, res) => {
+    const users = await usersService.getAll();
+    await res.json(users.map(User.toResponse));
+  })
+);
+
+router.route('/:id').get(
+  wrapAsync(async (req, res) => {
+    const user = await usersService.get(req.params.id);
+    res.status(200).send(User.toResponse(user));
+  })
+);
+
+router.route('/:id').delete(
+  wrapAsync(async (req, res) => {
+    await usersService.remove(req.params.id);
+    res.sendStatus(200);
+  })
+);
+
+router.route('/').post(
+  wrapAsync(async (req, res) => {
+    const user = await usersService.save(req.body);
+    res.status(200).send(User.toResponse(user));
+  })
+);
+
+router.route('/:id').put(
+  wrapAsync(async (req, res) => {
+    const user = await usersService.update(req.params.id, req.body);
+    res.status(200).send(User.toResponse(user));
+  })
+);
+
+function wrapAsync(fn) {
+  return (req, res, next) => {
+    // Make sure to `.catch()` any errors and pass them along to the `next()`
+    // middleware in the chain, in this case the error handler.
+    fn(req, res, next).catch(next);
+  };
+}
 
 module.exports = router;
