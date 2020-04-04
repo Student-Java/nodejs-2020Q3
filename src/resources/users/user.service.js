dbd4b51d4810d5035f5b900cad82526c9865a01f
Student-Java <energoslav@gmail.com>
2020-04-04 13:57:14 -0300

    feat: add user logic

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,16 @@
+const User = require('./user.model');
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const get = id => usersRepo.get(id);
+
+const remove = id => usersRepo.remove(id);
+
+const save = user => {
+  return usersRepo.save(new User(user));
+};
+
+const update = (id, user) => usersRepo.update(id, user);
+
+module.exports = { getAll, get, remove, save, update };
