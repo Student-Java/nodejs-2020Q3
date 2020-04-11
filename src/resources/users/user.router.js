58ef80c5a7e66781820ed0675a46b79933f51484
Student-Java <energoslav@gmail.com>
2020-04-11 19:37:45 -0300

    refactor: change hard coded status codes with constants

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,45 @@
+const { OK, CREATED, NO_CONTENT } = require('http-status-codes');
 const router = require('express').Router();
 const User = require('./user.model');
-const usersService = require('./user.service');
+const userService = require('./user.service');
+const wrapAsync = require('../../utils/wrapAsync');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+router.route('/').get(
+  wrapAsync(async (req, res) => {
+    const users = await userService.getAll();
+    await res.status(OK).json(users.map(User.toResponse));
+  })
+);
+
+router.route('/:id').get(
+  wrapAsync(async (req, res) => {
+    const user = await userService.get(req.params.id);
+    res.status(OK).send(User.toResponse(user));
+  })
+);
+
+router.route('/:id').delete(
+  wrapAsync(async (req, res) => {
+    await userService.remove(req.params.id);
+    res.sendStatus(NO_CONTENT);
+  })
+);
+
+router.route('/').post(
+  wrapAsync(async (req, res) => {
+    const user = await userService.save(User.fromRequest(req.body));
+    res.status(CREATED).send(User.toResponse(user));
+  })
+);
+
+router.route('/:id').put(
+  wrapAsync(async (req, res) => {
+    const user = await userService.update(
+      req.params.id,
+      User.fromRequest(req.body)
+    );
+    res.status(CREATED).send(User.toResponse(user));
+  })
+);
 
 module.exports = router;
