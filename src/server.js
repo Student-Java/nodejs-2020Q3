b9a7e04736d8ecbb9aa239963f9d1663959d15a9
Student-Java <energoslav@gmail.com>
2020-04-10 22:47:48 -0300

    feat: add workaround to catch and log unhandled rejections

--- src/server.js
+++ src/server.js
@@ -4,3 +4,7 @@ const app = require('./app');
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
+
+process.on('unhandledRejection', reason => {
+  throw reason;
+});
