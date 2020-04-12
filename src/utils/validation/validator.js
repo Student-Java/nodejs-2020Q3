252313436346e4f3259e51f66ec537982982445f
Student-Java <energoslav@gmail.com>
2020-04-11 23:46:37 -0300

    feat: extract error message formatting to separate function

--- src/utils/validation/validator.js
+++ src/utils/validation/validator.js
@@ -1,15 +1,23 @@
 const Joi = require('joi');
 const { BAD_REQUEST, UNPROCESSABLE_ENTITY } = require('http-status-codes');
 
+const errorResponse = errors => {
+  return {
+    status: 'failed',
+    errors: errors.map(err => {
+      const { path, message } = err;
+      return { path, message };
+    })
+  };
+};
+
 const validator = (schema, property) => {
   return (req, res, next) => {
     const { error } = Joi.validate(req[property], schema);
-
     if (error) {
-      const message = error.details.map(i => i.message).join();
       res
         .status(property === 'body' ? UNPROCESSABLE_ENTITY : BAD_REQUEST)
-        .json({ error: message });
+        .json({ error: errorResponse(error.details) });
     } else {
       // eslint-disable-next-line callback-return
       next();
