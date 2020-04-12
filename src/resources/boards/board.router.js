943749309c520098cf18899bc657dd7132558e25
Student-Java <energoslav@gmail.com>
2020-04-11 23:22:32 -0300

    feat: add validation to routers

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -1,24 +1,31 @@
-const { OK, CREATED, NO_CONTENT } = require('http-status-codes');
+const { OK, NO_CONTENT } = require('http-status-codes');
 const router = require('express').Router();
 const Board = require('./board.model');
 const boardService = require('./board.service');
 const wrapAsync = require('../../utils/wrapAsync');
+const { id } = require('../../utils/validation/shemas');
+const validator = require('../../utils/validation/validator');
 
-router.route('/').get(
+router.get(
+  '/',
   wrapAsync(async (req, res) => {
     const boards = await boardService.getAll();
     await res.status(OK).json(boards);
   })
 );
 
-router.route('/:id').get(
+router.get(
+  '/:id',
+  validator(id, 'params'),
   wrapAsync(async (req, res) => {
     const board = await boardService.get(req.params.id);
     res.status(OK).send(board);
   })
 );
 
-router.route('/:id').delete(
+router.delete(
+  '/:id',
+  validator(id, 'params'),
   wrapAsync(async (req, res) => {
     await boardService.remove(req.params.id);
     res.sendStatus(NO_CONTENT);
@@ -28,16 +35,18 @@ router.route('/:id').delete(
 router.route('/').post(
   wrapAsync(async (req, res) => {
     const board = await boardService.save(Board.fromRequest(req.body));
-    res.status(CREATED).send(board);
+    res.status(OK).send(board);
   })
 );
 
-router.route('/:id').put(
+router.put(
+  '/:id',
+  validator(id, 'params'),
   wrapAsync(async (req, res) => {
     const board = await boardService.update(
       Board.fromRequest({ ...req.body, id: req.params.id })
     );
-    res.status(CREATED).send(board);
+    res.status(OK).send(board);
   })
 );
 
