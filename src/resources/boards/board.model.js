997ce91dc5183e0132a3e6aa50294f6cca3ef360
Student-Java <energoslav@gmail.com>
2020-04-11 23:45:40 -0300

    refactor: specify uuid version - v4

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -2,7 +2,7 @@ const uuid = require('uuid');
 
 class Board {
   constructor({
-    id = uuid(),
+    id = uuid.v4(),
     title = 'TITLE',
     columns = { id: null, title: '', order: 0 }
   } = {}) {
