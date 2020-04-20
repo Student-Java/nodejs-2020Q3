3e88ad58ed179a2dbbc6ef133f4c863588197019
Student-Java <energoslav@gmail.com>
2020-04-20 10:30:31 -0300

    feat: add mongo to project

--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
-const { PORT } = require('./common/config');
-const app = require('./app');
 const logger = require('./common/logging');
 
+// uncaughtException is been catching by Winston
 process.on('unhandledRejection', reason => {
   process.emit('uncaughtException', reason);
 });
 
-app.listen(PORT, () =>
-  logger.info(`App is running on http://localhost:${PORT}`)
+const mongoose = require('mongoose');
+const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
+const app = require('./app');
+
+mongoose.connect(MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:')).once(
+  'open',
+  () => {
+    logger.info('Successfully connect to DB');
+    app.listen(PORT, () =>
+      logger.info(`App is running on http://localhost:${PORT}`)
+    );
+  }
 );
