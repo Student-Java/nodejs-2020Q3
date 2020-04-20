bd0ae4def2c4e1ad8fdaf8583150c98a56b53eaa
Student-Java <energoslav@gmail.com>
2020-04-20 10:31:00 -0300

    refactor: improve error console message

--- src/errors/errorHandler.js
+++ src/errors/errorHandler.js
@@ -5,7 +5,7 @@ const handle = (err, req, res, next) => {
   if (err.status) {
     res.status(err.status).send(err.message);
   } else {
-    logger.error(err, err.stack);
+    logger.error(err.stack);
     res
       .status(INTERNAL_SERVER_ERROR)
       .send(getStatusText(INTERNAL_SERVER_ERROR));
